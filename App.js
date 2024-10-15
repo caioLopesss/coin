6a2f2ded72a3a494332012fc820315d9e1dd7323
Caio Lopes <caiolopeis95@gmail.com>
2024-10-15 16:34:49 -0300

    preparando api

--- App.js
+++ App.js
@@ -3,6 +3,28 @@ import CurrentPrice from "./src/components/CurrentPrice";
 import HistoryGraphic from "./src/components/HistoryGraphic";
 import QuotationList from "./src/components/QuotationList";
 import QuotationItems from "./src/components/QuotationList/QuotationsItems";
+
+function addZero(number) {
+  if (number <= 9) {
+    return "0" + number;
+  }
+  return number;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  const listLastDays = qtdDays;
+  const end_date = `${date.getFullYear()}-${addZero(
+    date.getMonth() + 1
+  )}-${addZero(date.getDate())}`;
+  date.setDate(data.getDate() - listLastDays);
+  const start_date = `${date.getFullYear()}-${addZero(
+    date.getMonth() + 1
+  )}-${addZero(date.getDate())}`;
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&${(end =
+    end_date)}`;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
