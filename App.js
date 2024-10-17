c51e17ac4c5fcae961cef62b1ca05f151c684d63
Caio Lopes <caiolopeis95@gmail.com>
2024-10-17 09:57:49 -0300

    valor e data estao retornando no console.log

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { StyleSheet, StatusBar, SafeAreaView, Platform } from "react-native";
 import CurrentPrice from "./src/components/CurrentPrice";
 import HistoryGraphic from "./src/components/HistoryGraphic";
@@ -17,15 +18,61 @@ function url(qtdDays) {
   const end_date = `${date.getFullYear()}-${addZero(
     date.getMonth() + 1
   )}-${addZero(date.getDate())}`;
-  date.setDate(data.getDate() - listLastDays);
+  date.setDate(date.getDate() - listLastDays);
   const start_date = `${date.getFullYear()}-${addZero(
     date.getMonth() + 1
   )}-${addZero(date.getDate())}`;
-  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&${(end =
-    end_date)}`;
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`;
 }
 
+async function getListCoins(url) {
+  let response = await fetch(url);
+  let returnApi = await response.json();
+  let selectListQuotations = returnApi.bpi;
+  const queryCoinsList = Object.keys(selectListQuotations).map((key) => {
+    return {
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key],
+    };
+  });
+  let data = queryCoinsList.reverse();
+  return data;
+}
+
+const getPriceCoinsGraphic = async (url) => {
+  let responseG = await fetch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi;
+  const queryCoinsList = Object.keys(selectListQuotationsG).map((key) => {
+    return selectListQuotationsG[key];
+  });
+  let dataG = queryCoinsList;
+  return dataG;
+};
+
 export default function App() {
+  const [coinsList, setCoinsList] = useState([]);
+  const [coinsGraphicList, setCoinsGraphicList] = useState([0]);
+  const [days, setDays] = useState(30);
+  const [updateData, setUpdateData] = useState(true);
+
+  function updateDay(number) {
+    setDays(number);
+    setUpdateData(true);
+  }
+
+  useEffect(() => {
+    getListCoins(url(days)).then((data) => {
+      setCoinsList(data);
+    });
+    getPriceCoinsGraphic(url(days)).then((dataG) => {
+      setCoinsGraphicList(dataG);
+    });
+    if (updateData) {
+      setUpdateData(false);
+    }
+  }, [updateData]);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="#f50d41" barStyle="light-content" />
